2108692fc5503cce791c78efc0df54636b9dbc00
Platform23 <platform.kolonn@gmail.com>
2024-04-01 11:59:50 -0400

    chorebasic): setup layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,8 @@
-import Navbar from "../components/navigations/Navbar";
-
 const Home = () => {
   return (
-    <>
-      <Navbar />
-
-      <h1 className="text-3xl font-bold underline text-blue-700">
-        Hello world!
-      </h1>
-    </>
+    <h1 className="text-3xl font-bold underline text-blue-700">
+      Hello world!
+    </h1>
   )
 }
 
