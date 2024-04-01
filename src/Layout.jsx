1a051422dfdeb03e5fc731678f26e5dff3b21c6c
Platform23 <platform.kolonn@gmail.com>
2024-04-01 12:34:32 -0400

    chore(basic): setup layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,9 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import Sidebar from "./components/navigations/Sidebar";
 const Layout = () => {
     return (
         <>
+            <Sidebar />
             <Outlet />
         </>
     )
