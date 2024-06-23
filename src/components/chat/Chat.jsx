725e12b2f63110b4b8a82a7677a1d0c9e2112e11
Platform23 <platform.kolonn@gmail.com>
2024-06-23 19:58:17 -0400

    using InputEmoji instead

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -60,6 +60,7 @@ const Chat = () => {
                         placeholder="Entrer votre message"
                     />
                     
+                    {/* Send messge button */}
                     <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-primary hover:bg-bg-gray-700 focus:outline-none">
                         <span className="font-bold">Send</span>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
@@ -84,21 +85,6 @@ const Chat = () => {
                             className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                        </button> */}
-
-                        {/* Emoji Picker */}
-                        {/* {showEmojiPicker && (
-                            <div className="absolute bottom-12 right-0">
-                                <EmojiPicker onEmojiClick={(event, emojiObject) => onEmojiClick(emojiObject)} />
-                            </div>
-                        )} */}
-
-                        {/* Send messge button */}
-                        {/* <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-primary hover:bg-bg-gray-700 focus:outline-none">
-                            <span className="font-bold">Send</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
-                                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
                             </svg>
                         </button> */}
 
