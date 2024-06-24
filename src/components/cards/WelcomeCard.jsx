c10c26b1c183c318b4ebb12fcc61e7f9aecdc22a
Platform23 <platform.kolonn@gmail.com>
2024-06-24 19:00:14 -0400

    welcome screen fix

--- src/components/cards/WelcomeCard.jsx
+++ src/components/cards/WelcomeCard.jsx
@@ -1,19 +1,28 @@
+import { logo } from "../../assets/images"
 
 const WelcomeCard = () => {
 
     return (
-        <div className="p-4 border border-gray-300 rounded-lg bg-white text-primary items-center justify-center text-center">
-            <div className="mt-3">
-                <h3 className="text-2xl font-semibold">Bienvenue</h3>
-            </div>
+        <section className="items-center justify-center">
+            <img
+                className="mx-auto mb-7"
+                alt="Logo"
+                src={logo}
+            />
+            <div className="p-4 border border-gray-300 rounded-lg bg-white text-primary items-center justify-center text-center">
 
-            <div className="my-6 mb-4 text-xl">
-                Bienvenue dans Plaform. 
-                Veuillez à présent vérifier votre courriel 
-                pour activer votre compte et récupérer votre code d'intégration
-            </div>
+                <div className="mt-3">
+                    <h3 className="text-2xl font-semibold">Bienvenue</h3>
+                </div>
 
-        </div>
+                <div className="my-6 mb-4 text-xl">
+                    Bienvenue dans Plaform. 
+                    Veuillez à présent vérifier votre courriel 
+                    pour activer votre compte et récupérer votre code d'intégration
+                </div>
+
+            </div>
+        </section>
     )
 }
 
