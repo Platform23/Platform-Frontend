50c5c7078053157c25f0def7674b5222cc5394aa
Platform23 <147644622+Platform23@users.noreply.github.com>
2024-07-11 19:26:35 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -61,5 +61,5 @@ export const networks = [
     { name: "Technique et Professionnelle", description: "Centre de formation technique et professionnelle.", },
 ]
 
-export const API_BASE_URL = "http://backend:3333/api";
-export const BASE_URL = "http://backend:3333/api";
+export const API_BASE_URL = "https://api.platformht.com/api";
+export const BASE_URL = "https://api.platformht.com";
