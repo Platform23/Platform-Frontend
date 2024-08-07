55f3ff92469b2edae4759a3fc7dbc36bb4bda0f1
Platform23 <platform.kolonn@gmail.com>
2024-08-06 20:07:08 -0400

    password show feature

--- src/components/inputs/PasswordInput.jsx
+++ src/components/inputs/PasswordInput.jsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const PasswordInput = React.memo(({ placeholder, id, name, onChange }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
-        <div className="mt-5">
+        <div className="mt-5 relative">
             <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name={name}
                 id={id}
                 onChange={onChange}
                 placeholder={placeholder}
                 minLength="8"
                 className="w-full px-3 py-2 rounded-lg mt-2 border border-primary focus:outline-none"
-                required />
+                required
+            />
+            <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 pr-3 pt-2 flex items-center text-primary"
+            >
+                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </button>
         </div>
-    )
+    );
 });
 
 PasswordInput.displayName = 'PasswordInput';
 
-
-export default PasswordInput
+export default PasswordInput;
